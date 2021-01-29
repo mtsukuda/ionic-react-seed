6409dda80fde0bba20f0a3d21578f7feaaaa52c7
mtsukuda <mats.tsukuda@gmail.com>
2021-01-29 18:18:59 +0900

    implement gulp function: _cleanDirectories()

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const _ = require('lodash');
 const gulp = require('gulp');
 const FS = require('fs');
+const USER_COMPONENT_DIST = 'src/user-components';
 
 /**
  * Create User Components
  */
 gulp.task('create-user-components', function (done) {
+  _cleanDirectories(USER_COMPONENT_DIST);
   done();
 });
 
@@ -28,3 +30,23 @@ gulp.task('default',
   })
 );
 
+let _cleanDirectories = function (targetPath) {
+  _deleteDirectoryRecursive(targetPath);
+  FS.mkdirSync(targetPath, (err) =>{
+    console.log(err);
+  });
+}
+
+let _deleteDirectoryRecursive = function(path) {
+  if(FS.existsSync(path)) {
+    FS.readdirSync(path).forEach(function(file) {
+      let curPath = path + "/" + file;
+      if(FS.lstatSync(curPath).isDirectory()) { // recurse
+        _deleteDirectoryRecursive(curPath);
+      } else { // delete file
+        FS.unlinkSync(curPath);
+      }
+    });
+    FS.rmdirSync(path);
+  }
+};
