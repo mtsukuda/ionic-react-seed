93c53a508330ef502462cfc0abb17e2f5788fb16
mtsukuda <mats.tsukuda@gmail.com>
2021-03-14 20:52:16 +0900

    implement gulp task: copy-sample-pages and copy-sample-components.

--- gulpfile-createsample.js
+++ gulpfile-createsample.js
@@ -3,17 +3,72 @@ const FS = require('fs');
 const chalk = require('chalk');
 const MENU_SAMPLE_CONFIG_JSON = 'seed/app/menu.json.sample';
 const MENU_CONFIG_JSON = 'seed/app/menu.json';
+const PAGE_SAMPLE_CONFIG_JSON = 'seed/user-pages/sample-page';
+const COMPONENT_SAMPLE_CONFIG_JSON = 'seed/user-components/sample-component';
 
 /**
- * Create Menu
+ * Copy Menu
  */
 gulp.task('copy-menu', function (done){
   console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' copy-menu ') + ' 🚀🚀🚀 ');
-  if(FS.existsSync(MENU_CONFIG_JSON)) {
-    done(`Already exist ${MENU_CONFIG_JSON}.`);
-    return;
-  }
+  // if(FS.existsSync(MENU_CONFIG_JSON)) {
+  //   done(`Already exist ${MENU_CONFIG_JSON}.`);
+  //   return;
+  // }
   FS.copyFileSync(MENU_SAMPLE_CONFIG_JSON, MENU_CONFIG_JSON);
+  done();
+});
+
+/**
+ * Copy Sample Pages
+ */
+gulp.task('copy-sample-pages', function (done){
+  console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' copy-menu ') + ' 🚀🚀🚀 ');
+  let samplePages = [
+    `${PAGE_SAMPLE_CONFIG_JSON}1`,
+    `${PAGE_SAMPLE_CONFIG_JSON}2`
+  ];
+  const jsonSampleExt = 'json.sample';
+  samplePages.forEach((samplePage) => {
+    if(FS.existsSync(`${samplePage}.${jsonSampleExt}`) === false) {
+      done(`Could not find ${samplePage}.${jsonSampleExt}.`);
+    }
+  });
+  // samplePages.forEach((samplePage) => {
+  //   if(FS.existsSync(`${samplePage}.json`)) {
+  //     done(`Already exist ${samplePage}.json.`);
+  //   }
+  // });
+  samplePages.forEach((samplePage) => {
+    FS.copyFileSync(`${samplePage}.${jsonSampleExt}`, `${samplePage}.json`);
+  });
+  done();
+});
+
+
+/**
+ * Copy Sample Components
+ */
+gulp.task('copy-sample-components', function (done){
+  console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' copy-menu ') + ' 🚀🚀🚀 ');
+  let sampleComponents = [
+    `${COMPONENT_SAMPLE_CONFIG_JSON}1`,
+    `${COMPONENT_SAMPLE_CONFIG_JSON}2`
+  ];
+  const jsonSampleExt = 'json.sample';
+  sampleComponents.forEach((sampleComponent) => {
+    if(FS.existsSync(`${sampleComponent}.${jsonSampleExt}`) === false) {
+      done(`Could not find ${sampleComponent}.${jsonSampleExt}.`);
+    }
+  });
+  // sampleComponents.forEach((sampleComponent) => {
+  //   if(FS.existsSync(`${sampleComponent}.json`)) {
+  //     done(`Already exist ${sampleComponent}.json.`);
+  //   }
+  // });
+  sampleComponents.forEach((sampleComponent) => {
+    FS.copyFileSync(`${sampleComponent}.${jsonSampleExt}`, `${sampleComponent}.json`);
+  });
   done();
 });
 
@@ -23,6 +78,8 @@ gulp.task('copy-menu', function (done){
 gulp.task('default',
   gulp.series(gulp.parallel(
     'copy-menu',
+    'copy-sample-pages',
+    'copy-sample-components',
   ), function (done) {
     done();
   })
