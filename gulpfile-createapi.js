9a6c4cc96ec9023092eaec9fe200888e0fc493b7
mtsukuda <mats.tsukuda@gmail.com>
2021-03-19 16:37:25 +0900

    implement gulp task: create-api.

--- gulpfile-createapi.js
+++ gulpfile-createapi.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp');
 const FS = require('fs');
 const chalk = require('chalk');
+const gulpfs = require('./gulplib/gulpfs');
+const USER_PAGE_JSON = 'seed/user-pages';
+const USER_COMPONENT_JSON = 'seed/user-components';
 
 /**
  * Copy Menu
  */
 gulp.task('create-api', function (done){
   console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' create-api ') + ' 🚀🚀🚀 ');
+  let userPagesJSONFilePaths = gulpfs.jsonFilePaths(USER_PAGE_JSON);
+  let userComponentsJSONFilePaths = gulpfs.jsonFilePaths(USER_COMPONENT_JSON);
   done();
 });
 
