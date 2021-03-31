3bc3167455ce1631a125c6dbae2311d7e189a7c4
mtsukuda <mats.tsukuda@gmail.com>
2021-03-31 17:46:38 +0900

    implement gulp local function: _checkSlsPath().

--- gulpfile-pullendpoint.js
+++ gulpfile-pullendpoint.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const chalk = require('chalk');
+const gulpfs = require('./gulplib/gulpfs');
+const SLSPATH = 'seed/app/slspath';
 
 /**
  * API update
  */
 gulp.task('pull-endpoint', function (done){
   console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' pull-endpoint ') + ' 🚀🚀🚀 ');
+  _checkSlsPath();
   done();
 });
 
@@ -20,3 +23,15 @@ gulp.task('default',
   })
 );
 
+let _checkSlsPath = function () {
+  if(gulpfs.fileExists(SLSPATH) === false) {
+    let slsPath = { OwnSlsApiPath: "" };
+    gulpfs.writeDistFile(SLSPATH, JSON.stringify(slsPath, null, 2));
+  } else {
+    let slsPath = JSON.parse(gulpfs.readWholeFile(SLSPATH));
+    console.log(slsPath.OwnSlsApiPath);
+    if (!slsPath.OwnSlsApiPath) {
+      throw "Require SLS path -> seed/app/slspath";
+    }
+  }
+};
