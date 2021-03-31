86687f0a7cb360057f9e32bc3691a426d1b61f91
mtsukuda <mats.tsukuda@gmail.com>
2021-03-31 17:54:37 +0900

    enhance gulp local function: _checkSlsPath().

--- gulpfile-pullendpoint.js
+++ gulpfile-pullendpoint.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const chalk = require('chalk');
 const gulpfs = require('./gulplib/gulpfs');
 const SLSPATH = 'seed/app/slspath';
+const SLSSTACKPATH = '.serverless/stack.json';
 
 /**
  * API update
@@ -25,13 +26,18 @@ gulp.task('default',
 
 let _checkSlsPath = function () {
   if(gulpfs.fileExists(SLSPATH) === false) {
-    let slsPath = { OwnSlsApiPath: "" };
+    let slsPath = {OwnSlsApiPath: ""};
     gulpfs.writeDistFile(SLSPATH, JSON.stringify(slsPath, null, 2));
-  } else {
-    let slsPath = JSON.parse(gulpfs.readWholeFile(SLSPATH));
-    console.log(slsPath.OwnSlsApiPath);
-    if (!slsPath.OwnSlsApiPath) {
-      throw "Require SLS path -> seed/app/slspath";
-    }
+    return;
   }
+  let slsPath = JSON.parse(gulpfs.readWholeFile(SLSPATH));
+  console.log(slsPath.OwnSlsApiPath);
+  if (!slsPath.OwnSlsApiPath) {
+    throw "Require SLS path -> seed/app/slspath";
+  }
+  if(gulpfs.fileExists(`${slsPath.OwnSlsApiPath}/${SLSSTACKPATH}`) === false) {
+    throw `Could not find SLS stack -> ${slsPath.OwnSlsApiPath}/${SLSSTACKPATH}`;
+  }
+  let slsStackJSON = JSON.parse(gulpfs.readWholeFile(`${slsPath.OwnSlsApiPath}/${SLSSTACKPATH}`));
+  console.log(slsStackJSON);
 };
