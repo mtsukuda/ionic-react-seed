60fc8fe1ab0c151a6fbbfb580049461846e3e9ce
mtsukuda <mats.tsukuda@gmail.com>
2021-04-01 19:46:37 +0900

    refactor: rename SLS_CONFIG_JSON_PATH -> FRONT_API_CONFIG_JSON_PATH.

--- gulpcmd/cmd-pull-endpoint.js
+++ gulpcmd/cmd-pull-endpoint.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const chalk = require('chalk');
 const gulpfs = require('../gulplib/gulpfs');
-const SLS_CONFIG_JSON_PATH = '../seed/app/sls-config.json';
+const FRONT_API_CONFIG_JSON_PATH = '../seed/app/front-api-config.json';
 const SLS_STACK_JSON_PATH = '.serverless/stack.json';
 
 /**
@@ -29,11 +29,11 @@ gulp.task('default',
 );
 
 let _checkSlsPath = function (defaultApiPath) {
-  if(gulpfs.fileExists(SLS_CONFIG_JSON_PATH) === false) {
+  if(gulpfs.fileExists(FRONT_API_CONFIG_JSON_PATH) === false) {
     let slsPath = { FrontApiProjectPath: defaultApiPath, FrontApiEndPoint: ""};
-    gulpfs.writeDistFile(SLS_CONFIG_JSON_PATH, JSON.stringify(slsPath, null, 2));
+    gulpfs.writeDistFile(FRONT_API_CONFIG_JSON_PATH, JSON.stringify(slsPath, null, 2));
   }
-  let slsPath = JSON.parse(gulpfs.readWholeFile(SLS_CONFIG_JSON_PATH));
+  let slsPath = JSON.parse(gulpfs.readWholeFile(FRONT_API_CONFIG_JSON_PATH));
   if (slsPath.FrontApiProjectPath) {
     if (gulpfs.fileExists(`${slsPath.FrontApiProjectPath}/${SLS_STACK_JSON_PATH}`) === false) {
       throw new Error(`Could not find SLS stack -> ${slsPath.FrontApiProjectPath}/${SLS_STACK_JSON_PATH}`);
@@ -43,12 +43,12 @@ let _checkSlsPath = function (defaultApiPath) {
       throw new Error("Require FrontApiEndPoint in stack file");
     }
     slsPath['FrontApiEndPoint'] = slsStackJSON.FrontApiEndPoint;
-    gulpfs.writeDistFile(SLS_CONFIG_JSON_PATH, JSON.stringify(slsPath, null, 2));
+    gulpfs.writeDistFile(FRONT_API_CONFIG_JSON_PATH, JSON.stringify(slsPath, null, 2));
   } else {
-    throw new Error(`Require SLS path -> ${SLS_CONFIG_JSON_PATH}`);
+    throw new Error(`Require SLS path -> ${FRONT_API_CONFIG_JSON_PATH}`);
   }
 };
 
 exports.slsConfigJsonPath = () => {
-  return SLS_CONFIG_JSON_PATH.replace('../', '');
+  return FRONT_API_CONFIG_JSON_PATH.replace('../', '');
 }
