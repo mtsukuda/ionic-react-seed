b3828cb51ba19db4d15b027b9a77296bc846323c
mtsukuda <mats.tsukuda@gmail.com>
2021-04-02 23:55:44 +0900

    renamed & moved: gulpfile-apiupdate.js -> gulpcmd/cmd-api-update.js.

--- gulpcmd/cmd-api-update.js
+++ gulpcmd/cmd-api-update.js
@@ -1,15 +1,15 @@
 const _ = require('lodash');
 const gulp = require('gulp');
 const chalk = require('chalk');
-const gulpfs = require('./gulplib/gulpfs');
-const USER_PAGE_JSON = 'seed/user-pages';
-const USER_COMPONENT_JSON = 'seed/user-components';
+const gulpfs = require('../gulplib/gulpfs');
+const USER_PAGE_JSON = '../seed/user-pages';
+const USER_COMPONENT_JSON = '../seed/user-components';
 
 /**
  * API update
  */
 gulp.task('api-update', function (done){
-  console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' create-api ') + ' 🚀🚀🚀 ');
+  console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' api-update ') + ' 🚀🚀🚀 ');
   let userPagesJSONFilePaths = gulpfs.jsonFilePaths(USER_PAGE_JSON);
   let fetchData = [];
   if (userPagesJSONFilePaths !== null && userPagesJSONFilePaths.length > 0) {
