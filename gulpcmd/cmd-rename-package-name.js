ac6ac058111f5671284bb75e8a7b011c484bb503
mtsukuda <mats.tsukuda@gmail.com>
2021-04-04 21:41:25 +0900

    add gulp command: rename-firebase-resource-name.

--- gulpcmd/cmd-rename-package-name.js
+++ gulpcmd/cmd-rename-package-name.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const gulpfs = require('../gulplib/gulpfs');
 const PACKAGE_JSON = 'package.json';
 const IONIC_CONFIG_JSON = 'ionic.config.json';
+const FIREBASE_RESOURCE_JSON = '.firebaserc';
 
 /**
  * Rename Package Name
@@ -18,7 +19,6 @@ gulp.task('rename-package-name', async function (done){
   gulpfs.writeDistFile(ionicProjectPackageJSONPath, JSON.stringify(packageJSON, null, 2));
   done();
 });
-
 
 /**
  * Rename Ionic Config Name
@@ -36,12 +36,28 @@ gulp.task('rename-ionic-config-name', async function (done){
 });
 
 /**
+ * Rename Firebase Resource Name
+ */
+gulp.task('rename-firebase-resource-name', async function (done){
+  console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' rename-firebase-resource-name ') + ' 🚀🚀🚀 ');
+  let firebaseResourceJSONPath = `../${FIREBASE_RESOURCE_JSON}`;
+  let firebaseResourceJSON = gulpfs.JSONdata(firebaseResourceJSONPath, false);
+  let dirArray = __dirname.split('/');
+  let newPackageName = dirArray[dirArray.length - 2];
+  console.log(`RENAMED FIREBASE RESOURCE: ${firebaseResourceJSON.projects.default} ==> ${newPackageName}`);
+  firebaseResourceJSON.projects.default = newPackageName;
+  gulpfs.writeDistFile(firebaseResourceJSONPath, JSON.stringify(firebaseResourceJSON, null, 2));
+  done();
+});
+
+/**
  * gulp default task
  */
 gulp.task('default',
   gulp.series(gulp.parallel(
     'rename-package-name',
     'rename-ionic-config-name',
+    'rename-firebase-resource-name',
   ), function (done) {
     done();
   })
