17e03fe7413f5ea0421968b4b18be705e14190de
mtsukuda <mats.tsukuda@gmail.com>
2021-04-16 09:52:07 +0900

    create gulp log common.

---
+++ gulplib/gulplog.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+
+module.exports.logKeyVal = (key, val, separate=' ') => {
+  return chalk.magenta(`${key}`) + separate + val ;
+}
+
+module.exports.logKeyValYesNo = (key, val, separate=' ') => {
+  return chalk.magenta(`${key}`) + separate + (val ? chalk.green(`yes`) :  chalk.red(`no`)) ;
+}
