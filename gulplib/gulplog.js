983df883819d65f9ee53a6e0043d4e444e3f3856
mtsukuda <mats.tsukuda@gmail.com>
2021-04-18 05:30:13 +0900

    refactor: prettier sorce code.

--- gulplib/gulplog.js
+++ gulplib/gulplog.js
@@ -1,9 +1,13 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
 
-module.exports.logKeyVal = (key, val, separate=' ') => {
-  return chalk.magenta(`${key}`) + separate + val ;
-}
+module.exports.logKeyVal = (key, val, separate = " ") => {
+  return chalk.magenta(`${key}`) + separate + val;
+};
 
-module.exports.logKeyValYesNo = (key, val, separate=' ') => {
-  return chalk.magenta(`${key}`) + separate + (val ? chalk.green(`yes`) :  chalk.red(`no`)) ;
-}
+module.exports.logKeyValYesNo = (key, val, separate = " ") => {
+  return (
+    chalk.magenta(`${key}`) +
+    separate +
+    (val ? chalk.green(`yes`) : chalk.red(`no`))
+  );
+};
