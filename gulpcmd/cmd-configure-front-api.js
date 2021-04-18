173ab07c5a8300902b27a0c1b6d24955236dfd5c
mtsukuda <mats.tsukuda@gmail.com>
2021-04-19 08:49:25 +0900

    refactor: prettier sorce code.

--- gulpcmd/cmd-configure-front-api.js
+++ gulpcmd/cmd-configure-front-api.js
@@ -1,19 +1,19 @@
-const _ = require('lodash');
-const gulp = require('gulp');
-const chalk = require('chalk');
-const gulpFs = require('../gulplib/gulpfs');
-const gulpConst = require('../gulplib/gulpconst');
-const gulpLog = require('../gulplib/gulplog');
+const _ = require("lodash");
+const gulp = require("gulp");
+const chalk = require("chalk");
+const gulpFs = require("../gulplib/gulpfs");
+const gulpConst = require("../gulplib/gulpconst");
+const gulpLog = require("../gulplib/gulplog");
 const gulpHeadLine = require("./gulplib/gulpheadline");
-const pullEndPoint = require('../gulpcmd/cmd-pull-endpoint');
-const USER_PAGE_JSON = '../seed/user-pages';
-const USER_COMPONENT_JSON = '../seed/user-components';
-const FRONT_API_FUNCTIONS_CONFIG = 'seed/functions/config.json';
+const pullEndPoint = require("../gulpcmd/cmd-pull-endpoint");
+const USER_PAGE_JSON = "../seed/user-pages";
+const USER_COMPONENT_JSON = "../seed/user-components";
+const FRONT_API_FUNCTIONS_CONFIG = "seed/functions/config.json";
 
 /**
  * Configure Front API
  */
-gulp.task('configure-front-api', function (done){
+gulp.task("configure-front-api", function (done) {
   gulpHeadLine.taskNameWrite("configure-front-api");
   let projectPath = _frontApiProjectPath();
   if (projectPath === false) {
@@ -25,21 +25,27 @@ gulp.task('configure-front-api', function (done){
     _extractFetchData(userPagesJSONFilePaths, fetchData);
   }
   let userComponentsJSONFilePaths = gulpFs.jsonFilePaths(USER_COMPONENT_JSON);
-  if (userComponentsJSONFilePaths !== null && userComponentsJSONFilePaths.length > 0) {
+  if (
+    userComponentsJSONFilePaths !== null &&
+    userComponentsJSONFilePaths.length > 0
+  ) {
     _extractFetchData(userComponentsJSONFilePaths, fetchData);
   }
   let functionJSON = { functions: [] };
   _createApiData(fetchData, functionJSON);
   _configureLog(functionJSON);
   let frontApiFunctionsPath = `${projectPath}/${FRONT_API_FUNCTIONS_CONFIG}`;
-  gulpFs.writeDistFile(frontApiFunctionsPath, JSON.stringify(functionJSON, null, 2));
+  gulpFs.writeDistFile(
+    frontApiFunctionsPath,
+    JSON.stringify(functionJSON, null, 2)
+  );
   done();
 });
 
 /**
  * Verify Front API Response Type
  */
-gulp.task('verify-front-api-response-type', function (done){
+gulp.task("verify-front-api-response-type", function (done) {
   gulpHeadLine.taskNameWrite("verify-front-api-response-type");
   let userPagesJSONFilePaths = gulpFs.jsonFilePaths(USER_PAGE_JSON);
   let fetchData = [];
@@ -47,7 +53,10 @@ gulp.task('verify-front-api-response-type', function (done){
     _extractFetchData(userPagesJSONFilePaths, fetchData);
   }
   let userComponentsJSONFilePaths = gulpFs.jsonFilePaths(USER_COMPONENT_JSON);
-  if (userComponentsJSONFilePaths !== null && userComponentsJSONFilePaths.length > 0) {
+  if (
+    userComponentsJSONFilePaths !== null &&
+    userComponentsJSONFilePaths.length > 0
+  ) {
     _extractFetchData(userComponentsJSONFilePaths, fetchData);
   }
   _compareResponseType(fetchData);
@@ -57,20 +66,21 @@ gulp.task('verify-front-api-response-type', function (done){
 /**
  * gulp default task
  */
-gulp.task('default',
-  gulp.series(gulp.parallel(
-    'configure-front-api',
-  ), function (done) {
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("configure-front-api"), function (done) {
     done();
   })
 );
 
 let _frontApiProjectPath = function () {
   let frontApiConfigJsonPath = `../${pullEndPoint.frontApiConfigJsonPath()}`;
-  if(gulpFs.fileExists(frontApiConfigJsonPath) === false) {
+  if (gulpFs.fileExists(frontApiConfigJsonPath) === false) {
     return false;
   }
-  let frontApiConfigJson = JSON.parse(gulpFs.readWholeFile(frontApiConfigJsonPath));
+  let frontApiConfigJson = JSON.parse(
+    gulpFs.readWholeFile(frontApiConfigJsonPath)
+  );
   if (!frontApiConfigJson) {
     return false;
   }
@@ -79,9 +89,12 @@ let _frontApiProjectPath = function () {
 
 let _extractFetchData = function (componentConfigJSONFilePaths, fetchData) {
   _.forEach(componentConfigJSONFilePaths, (componentConfigJSONFilePath) => {
-    let componentConfigJSON = gulpFs.JSONdata(componentConfigJSONFilePath, false);
+    let componentConfigJSON = gulpFs.JSONdata(
+      componentConfigJSONFilePath,
+      false
+    );
     _.forEach(componentConfigJSON, (value, key) => {
-      if (key === 'fetch') {
+      if (key === "fetch") {
         _.forEach(value, (fetch) => {
           fetchData.push(fetch);
         });
@@ -94,13 +107,14 @@ let _createApiData = function (fetchData, functionJSON) {
   _.forEach(fetchData, (fetch) => {
     _.forEach(fetch.apis, (api) => {
       if (gulpConst.slsFrontApiUri() === api.uri) {
-        if (api.config.path === undefined) throw new Error("Could not find 'api.config.path'!");
+        if (api.config.path === undefined)
+          throw new Error("Could not find 'api.config.path'!");
         let functionConfig = { method: fetch.method, path: api.config.path };
         if (api.config.mock) {
-          functionConfig['mock'] = api.config.mock;
+          functionConfig["mock"] = api.config.mock;
         }
         if (api.config.schema) {
-          functionConfig['schema'] = api.config.schema;
+          functionConfig["schema"] = api.config.schema;
         }
         functionJSON.functions.push(functionConfig);
       }
@@ -123,7 +137,9 @@ let _compareResponseType = function (fetchData) {
       flatArrayResponseType.sort();
       flatArrayMockData.sort();
       if (_.isEqual(flatArrayResponseType, flatArrayMockData) === false) {
-        throw new Error(`Could not mach between response type and mock data: ${api.responseTypeName}`);
+        throw new Error(
+          `Could not mach between response type and mock data: ${api.responseTypeName}`
+        );
       }
       console.log(chalk.greenBright(`✓ `) + `${api.responseTypeName}`);
     });
@@ -131,22 +147,27 @@ let _compareResponseType = function (fetchData) {
   return true;
 };
 
-let _extractArrayKeys = function (responseType, flatArray, parentKey='') {
+let _extractArrayKeys = function (responseType, flatArray, parentKey = "") {
   _.forEach(responseType, (value, key) => {
     if (_.isObject(value)) {
-      _extractArrayKeys(value, flatArray, (parentKey?`${parentKey}-`:'') + key);
+      _extractArrayKeys(
+        value,
+        flatArray,
+        (parentKey ? `${parentKey}-` : "") + key
+      );
     }
-    flatArray.push((parentKey ? `${parentKey}-` : '') + key);
-  })
+    flatArray.push((parentKey ? `${parentKey}-` : "") + key);
+  });
 };
 
 let _configureLog = function (functionJSON) {
   console.log(chalk.green(`functions: `));
   functionJSON.functions.forEach((func) => {
-    let log = `${gulpLog.logKeyVal('path', func.path)} `
-            + `${gulpLog.logKeyVal('method', func.method)} `
-            + `${gulpLog.logKeyValYesNo('mock', func.mock)} `
-            + `${gulpLog.logKeyValYesNo('schema', func.schema)} `;
-    console.log('  ' + log);
+    let log =
+      `${gulpLog.logKeyVal("path", func.path)} ` +
+      `${gulpLog.logKeyVal("method", func.method)} ` +
+      `${gulpLog.logKeyValYesNo("mock", func.mock)} ` +
+      `${gulpLog.logKeyValYesNo("schema", func.schema)} `;
+    console.log("  " + log);
   });
 };
