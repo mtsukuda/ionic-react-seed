96ddb1a16a811394021f8633fc3e3406e9a43093
mtsukuda <mats.tsukuda@gmail.com>
2021-04-19 09:16:18 +0900

    refactor: prettier sorce code.

--- gulpcmd/cmd-overwrite-fireabase-json.js
+++ gulpcmd/cmd-overwrite-fireabase-json.js
@@ -1,25 +1,27 @@
-const gulp = require('gulp');
-const gulpFs = require('../gulplib/gulpfs');
+const gulp = require("gulp");
+const gulpFs = require("../gulplib/gulpfs");
 const gulpHeadLine = require("../gulplib/gulpheadline");
-const FIREBASE_JSON = 'firebase.json';
+const FIREBASE_JSON = "firebase.json";
 
 /**
  * Overwrite Firebase JSON File
  */
-gulp.task('overwrite-firebase-json', async function (done){
+gulp.task("overwrite-firebase-json", async function (done) {
   gulpHeadLine.taskNameWrite("overwrite-firebase-json");
   let packageJSON = gulpFs.JSONdata(`../seed/config/${FIREBASE_JSON}`, false);
-  gulpFs.writeDistFile(`../${FIREBASE_JSON}`, JSON.stringify(packageJSON, null, 2));
+  gulpFs.writeDistFile(
+    `../${FIREBASE_JSON}`,
+    JSON.stringify(packageJSON, null, 2)
+  );
   done();
 });
 
 /**
  * gulp default task
  */
-gulp.task('default',
-  gulp.series(gulp.parallel(
-    'overwrite-firebase-json',
-  ), function (done) {
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("overwrite-firebase-json"), function (done) {
     done();
   })
 );
