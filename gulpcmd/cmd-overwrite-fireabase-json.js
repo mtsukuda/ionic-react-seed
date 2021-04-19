afa62969fb63df13a41779d4c0945ef3f4d35f5f
mtsukuda <mats.tsukuda@gmail.com>
2021-04-19 09:14:03 +0900

    refactor: gulpfs --> gulpFs, console.log(🚀) --> gulpHeadLine.taskNameWrite().

--- gulpcmd/cmd-overwrite-fireabase-json.js
+++ gulpcmd/cmd-overwrite-fireabase-json.js
@@ -1,15 +1,15 @@
 const gulp = require('gulp');
-const chalk = require('chalk');
-const gulpfs = require('../gulplib/gulpfs');
+const gulpFs = require('../gulplib/gulpfs');
+const gulpHeadLine = require("../gulplib/gulpheadline");
 const FIREBASE_JSON = 'firebase.json';
 
 /**
  * Overwrite Firebase JSON File
  */
 gulp.task('overwrite-firebase-json', async function (done){
-  console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' overwrite-firebase-json ') + ' 🚀🚀🚀 ');
-  let packageJSON = gulpfs.JSONdata(`../seed/config/${FIREBASE_JSON}`, false);
-  gulpfs.writeDistFile(`../${FIREBASE_JSON}`, JSON.stringify(packageJSON, null, 2));
+  gulpHeadLine.taskNameWrite("overwrite-firebase-json");
+  let packageJSON = gulpFs.JSONdata(`../seed/config/${FIREBASE_JSON}`, false);
+  gulpFs.writeDistFile(`../${FIREBASE_JSON}`, JSON.stringify(packageJSON, null, 2));
   done();
 });
 
