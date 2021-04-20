bbb593b00b47e1e3f247896c134b0a0d4d3b27f0
mtsukuda <mats.tsukuda@gmail.com>
2021-04-20 09:17:02 +0900

    refactor: prettier sorce code.

--- gulpcmd/cmd-rename-package-name.js
+++ gulpcmd/cmd-rename-package-name.js
@@ -1,50 +1,57 @@
-const gulp = require('gulp');
-const gulpFs = require('../gulplib/gulpfs');
+const gulp = require("gulp");
+const gulpFs = require("../gulplib/gulpfs");
 const gulpHeadLine = require("../gulplib/gulpheadline");
-const PACKAGE_JSON = 'package.json';
-const IONIC_CONFIG_JSON = 'ionic.config.json';
+const PACKAGE_JSON = "package.json";
+const IONIC_CONFIG_JSON = "ionic.config.json";
 
 /**
  * Rename Package Name
  */
-gulp.task('rename-package-name', async function (done){
+gulp.task("rename-package-name", async function (done) {
   gulpHeadLine.taskNameWrite("rename-package-name");
   let ionicProjectPackageJSONPath = `../${PACKAGE_JSON}`;
   let packageJSON = gulpFs.JSONdata(ionicProjectPackageJSONPath, false);
   let newPackageName = _newPackageName();
   console.log(`RENAMED PACKAGE: ${packageJSON.name} ==> ${newPackageName}`);
   packageJSON.name = newPackageName;
-  gulpFs.writeDistFile(ionicProjectPackageJSONPath, JSON.stringify(packageJSON, null, 2));
+  gulpFs.writeDistFile(
+    ionicProjectPackageJSONPath,
+    JSON.stringify(packageJSON, null, 2)
+  );
   done();
 });
 
 /**
  * Rename Ionic Config Name
  */
-gulp.task('rename-ionic-config-name', async function (done){
+gulp.task("rename-ionic-config-name", async function (done) {
   gulpHeadLine.taskNameWrite("rename-ionic-config-name");
   let ionicConfigJSONPath = `../${IONIC_CONFIG_JSON}`;
   let configJSON = gulpFs.JSONdata(ionicConfigJSONPath, false);
   let newPackageName = _newPackageName();
   console.log(`RENAMED IONIC CONFIG: ${configJSON.name} ==> ${newPackageName}`);
   configJSON.name = newPackageName;
-  gulpFs.writeDistFile(ionicConfigJSONPath, JSON.stringify(configJSON, null, 2));
+  gulpFs.writeDistFile(
+    ionicConfigJSONPath,
+    JSON.stringify(configJSON, null, 2)
+  );
   done();
 });
 
 /**
  * gulp default task
  */
-gulp.task('default',
-  gulp.series(gulp.parallel(
-    'rename-package-name',
-    'rename-ionic-config-name',
-  ), function (done) {
-    done();
-  })
+gulp.task(
+  "default",
+  gulp.series(
+    gulp.parallel("rename-package-name", "rename-ionic-config-name"),
+    function (done) {
+      done();
+    }
+  )
 );
 
 let _newPackageName = function () {
-  let dirArray = __dirname.split('/');
+  let dirArray = __dirname.split("/");
   return dirArray[dirArray.length - 2];
-}
+};
