3468291b93ea4ffd762b998feb7ff35e7f06c8a1
mtsukuda <mats.tsukuda@gmail.com>
2021-04-20 09:26:12 +0900

    refactor: prettier sorce code.

--- gulpsh/sh-frontapi-git-clone.js
+++ gulpsh/sh-frontapi-git-clone.js
@@ -1,29 +1,32 @@
-const gulp = require('gulp');
-const chalk = require('chalk');
+const gulp = require("gulp");
+const chalk = require("chalk");
 const exec = require("exec-sh").promise;
-const gulpFs = require('../gulplib/gulpfs');
-const pullEndPoint = require('../gulpcmd/cmd-pull-endpoint');
+const gulpFs = require("../gulplib/gulpfs");
+const pullEndPoint = require("../gulpcmd/cmd-pull-endpoint");
 const gulpHeadLine = require("../gulplib/gulpheadline");
 
 /**
  * Front Api Seed git clone
  */
-gulp.task('git-clone', async function (done){
+gulp.task("git-clone", async function (done) {
   gulpHeadLine.taskNameWrite("git-clone");
-  let packageJSON = gulpFs.JSONdata('../package.json', false);
+  let packageJSON = gulpFs.JSONdata("../package.json", false);
   let clonedName = `${packageJSON.name}-api`;
-  if(gulpFs.fileExists(`../../${clonedName}`)) {
+  if (gulpFs.fileExists(`../../${clonedName}`)) {
     console.log(chalk.red(`Already exist front API -> ${clonedName}`));
     done();
     return;
   }
   let out;
   try {
-    out = await exec(`git clone git@github.com:mtsukuda/sls-front-api-seed.git ../../${clonedName}`, true);
+    out = await exec(
+      `git clone git@github.com:mtsukuda/sls-front-api-seed.git ../../${clonedName}`,
+      true
+    );
   } catch (e) {
-    console.log('Error: ', e);
-    console.log('Stderr: ', e.stderr);
-    console.log('Stdout: ', e.stdout);
+    console.log("Error: ", e);
+    console.log("Stderr: ", e.stderr);
+    console.log("Stdout: ", e.stdout);
     return e;
   }
   console.log(out);
@@ -34,11 +37,9 @@ gulp.task('git-clone', async function (done){
 /**
  * gulp default task
  */
-gulp.task('default',
-  gulp.series(gulp.parallel(
-    'git-clone',
-  ), function (done) {
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("git-clone"), function (done) {
     done();
   })
 );
-
