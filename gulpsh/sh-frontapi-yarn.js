a512307739bd484d00fee545beacb1f22114c363
mtsukuda <mats.tsukuda@gmail.com>
2021-04-20 09:28:13 +0900

    refactor: prettier sorce code.

--- gulpsh/sh-frontapi-yarn.js
+++ gulpsh/sh-frontapi-yarn.js
@@ -1,23 +1,22 @@
-const gulp = require('gulp');
-const chalk = require('chalk');
+const gulp = require("gulp");
 const exec = require("exec-sh").promise;
-const gulpFs = require('../gulplib/gulpfs');
+const gulpFs = require("../gulplib/gulpfs");
 const gulpHeadLine = require("../gulplib/gulpheadline");
 
 /**
  * Front Api yarn
  */
-gulp.task('front-api-yarn', async function (done){
+gulp.task("front-api-yarn", async function (done) {
   gulpHeadLine.taskNameWrite("front-api-yarn");
-  let packageJSON = gulpFs.JSONdata('../package.json', false);
+  let packageJSON = gulpFs.JSONdata("../package.json", false);
   let apiPath = `../../${packageJSON.name}-api`;
   let out;
   try {
     out = await exec(`yarn install --cwd ${apiPath}`, true);
   } catch (e) {
-    console.log('Error: ', e);
-    console.log('Stderr: ', e.stderr);
-    console.log('Stdout: ', e.stdout);
+    console.log("Error: ", e);
+    console.log("Stderr: ", e.stderr);
+    console.log("Stdout: ", e.stdout);
     return e;
   }
   console.log(out);
@@ -27,11 +26,9 @@ gulp.task('front-api-yarn', async function (done){
 /**
  * gulp default task
  */
-gulp.task('default',
-  gulp.series(gulp.parallel(
-    'front-api-yarn',
-  ), function (done) {
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("front-api-yarn"), function (done) {
     done();
   })
 );
-
