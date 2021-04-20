7a496dd4e700898cb0af0126c4daeef6285e2d4e
mtsukuda <mats.tsukuda@gmail.com>
2021-04-20 09:30:03 +0900

    refactor: prettier sorce code.

--- gulpsh/sh-frontapi-yarn-run-create-functions.js
+++ gulpsh/sh-frontapi-yarn-run-create-functions.js
@@ -1,30 +1,38 @@
-const gulp = require('gulp');
-const chalk = require('chalk');
+const gulp = require("gulp");
 const exec = require("exec-sh").promise;
-const gulpFs = require('../gulplib/gulpfs');
+const gulpFs = require("../gulplib/gulpfs");
 const gulpHeadLine = require("../gulplib/gulpheadline");
-const FRONT_API_CONFIG_JSON_PATH = '../seed/app/front-api-config.json';
+const FRONT_API_CONFIG_JSON_PATH = "../seed/app/front-api-config.json";
 
 /**
  * Front Api yarn: run create-functions
  */
-gulp.task('front-api-yarn-run-create-functions', async function (done){
+gulp.task("front-api-yarn-run-create-functions", async function (done) {
   gulpHeadLine.taskNameWrite("front-api-yarn-run-create-functions");
-  if(gulpFs.fileExists(FRONT_API_CONFIG_JSON_PATH) === false) {
-    throw new Error(`Could not find api config json: ${FRONT_API_CONFIG_JSON_PATH}.`);
+  if (gulpFs.fileExists(FRONT_API_CONFIG_JSON_PATH) === false) {
+    throw new Error(
+      `Could not find api config json: ${FRONT_API_CONFIG_JSON_PATH}.`
+    );
   }
-  let frontApiConfig = JSON.parse(gulpFs.readWholeFile(FRONT_API_CONFIG_JSON_PATH));
+  let frontApiConfig = JSON.parse(
+    gulpFs.readWholeFile(FRONT_API_CONFIG_JSON_PATH)
+  );
   if (!frontApiConfig.FrontApiProjectPath) {
-    throw new Error(`Could not find the entory of FrontApiProjectPath from api config json.`);
+    throw new Error(
+      `Could not find the entory of FrontApiProjectPath from api config json.`
+    );
   }
   console.log(frontApiConfig.FrontApiProjectPath);
   let out;
   try {
-    out = await exec(`yarn --cwd ${frontApiConfig.FrontApiProjectPath} run create-functions`, true);
+    out = await exec(
+      `yarn --cwd ${frontApiConfig.FrontApiProjectPath} run create-functions`,
+      true
+    );
   } catch (e) {
-    console.log('Error: ', e);
-    console.log('Stderr: ', e.stderr);
-    console.log('Stdout: ', e.stdout);
+    console.log("Error: ", e);
+    console.log("Stderr: ", e.stderr);
+    console.log("Stdout: ", e.stdout);
     return e;
   }
   console.log(out);
@@ -34,11 +42,12 @@ gulp.task('front-api-yarn-run-create-functions', async function (done){
 /**
  * gulp default task
  */
-gulp.task('default',
-  gulp.series(gulp.parallel(
-    'front-api-yarn-run-create-functions',
-  ), function (done) {
-    done();
-  })
+gulp.task(
+  "default",
+  gulp.series(
+    gulp.parallel("front-api-yarn-run-create-functions"),
+    function (done) {
+      done();
+    }
+  )
 );
-
